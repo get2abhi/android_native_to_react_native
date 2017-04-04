920f1f377084ded8484a1fb72153785dad3b97f8
Abhinav Srivastava <abhinav.srivastava@olacabs.com>
2017-04-04 15:32:58 +0530

    Initial commit

---
+++ android/app/src/main/java/com/android_native_to_react_native/MainApplication.java
@@ -0,0 +1,60 @@
+package com.android_native_to_react_native;
+
+import android.app.Application;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage()
+            );
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainApplication.this, "started", Toast.LENGTH_LONG).show();
+                WritableMap params = Arguments.createMap();
+                params.putString("hello", "world");
+                sendEvent(mReactNativeHost.getReactInstanceManager().getCurrentReactContext(), "customevent", params);
+            }
+        }, 5000);
+    }
+
+    private void sendEvent(ReactContext reactContext, String eventName, @Nullable WritableMap params) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+    }
+}
